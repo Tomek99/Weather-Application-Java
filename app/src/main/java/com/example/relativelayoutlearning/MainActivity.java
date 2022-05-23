a71d1202ee5e49f380a03048f057099943a1d6f6
Tomasz Skupien <Szybko872@gmail.com>
2022-05-23 21:06:43 +0200

    21:06 finish

--- app/src/main/java/com/example/relativelayoutlearning/MainActivity.java
+++ app/src/main/java/com/example/relativelayoutlearning/MainActivity.java
@@ -3,21 +3,20 @@ package com.example.relativelayoutlearning;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -25,108 +24,120 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private static String city = "Jaroslaw";
     private static final String API = "8544c10e7dc1de8a48fd3f18dd3c13ed";
 
-    EditText getCity;
-    Button click;
+    private EditText getCity;
+    private OkHttpClient client;
+
+
+    private String location;
+    private Long sunset;
+    private Long sunrise;
+    private String windd;
+    private String description;
+    private String lastModified;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&lang=pl&appid=" + API;
+        getCity = (EditText) findViewById(R.id.enterLocation);
+        Button click = (Button) findViewById(R.id.searchLocation);
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    public void onResponse (JSONObject response) {
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWebservice(getCity.getText().toString());
+                getCity.setText("");
+            }
+        });
+        client = new OkHttpClient();
+    }
+
+
+    private void getWebservice(String city) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=pl&appid=" + API;
+        final Request request = new Request.Builder().url(url).build();
+
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String error = "Brak internetu";
+                        ((TextView) findViewById(R.id.location)).setText(error);
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
                         try {
-                            JSONObject main = response.getJSONObject("main");
-                            JSONObject sys = response.getJSONObject("sys");
-                            JSONObject wind = response.getJSONObject("wind");
-                            JSONObject weather = response.getJSONArray("weather").getJSONObject(0);
+                            String jsonData = response.body().string();
+                            JSONObject jObject = new JSONObject(jsonData);
 
-                            String lastModified = new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH)
-                                    .format(new Date(response.getLong("dt") * 1000));
+                            JSONObject main = jObject.getJSONObject("main");
+                            JSONObject sys = jObject.getJSONObject("sys");
+                            JSONObject wind = jObject.getJSONObject("wind");
+                            JSONObject weather = jObject.getJSONArray("weather").getJSONObject(0);
 
-
+                            lastModified = "Ostatnia akutlizacja: " + (new SimpleDateFormat("dd/MM/yyyy hh:mm a",
+                                    Locale.ENGLISH).format(new Date(jObject.getLong("dt") * 1000)));
 
                             //main
-                            Long temp = Math.round(main.getDouble("temp"));
-                            String temp_min = main.getString("temp_min");
-                            String temp_max = main.getString("temp_max");
+                            String temp = (Math.round(main.getDouble("temp"))) + "°C";
+                            String temp_min = "Min temp: " + (main.getString("temp_min")) + "°C";
+                            String temp_max = "Max temp: " + (main.getString("temp_max")) + "°C";
                             String pressure = main.getString("pressure");
-                            String humidity = main.getString("humidity");
+                            String humidity = (main.getString("humidity")) + " %";
 
-                            //sys
-                            String location = response.getString("name") + ", " + sys.getString("country");
+                            String location = jObject.getString("name") + ", " + sys.getString("country");
 
                             long sunrise = sys.getLong("sunrise");
                             long sunset = sys.getLong("sunset");
 
                             //wind
-                            String wind1 = wind.getString("speed");
+                            String windd = (wind.getString("speed")) + " km/h";
                             //weather
                             String description = weather.getString("description");
 
-                            ((TextView) findViewById(R.id.last_updated)).setText("Ostatnia aktualizacja: " +lastModified);
+                            ((TextView) findViewById(R.id.last_updated)).setText(lastModified);
                             ((TextView) findViewById(R.id.location)).setText(location);
 
                             ((TextView) findViewById(R.id.weatherDescription)).setText(description);
-                            ((TextView) findViewById(R.id.temperature)).setText(temp + "°C");
+                            ((TextView) findViewById(R.id.temperature)).setText(temp);
 
-                            ((TextView) findViewById(R.id.minTemp)).setText("Min temp: " + temp_min + "°C");
-                            ((TextView) findViewById(R.id.maxTemp)).setText("Max temp: " + temp_max + "°C");
+                            ((TextView) findViewById(R.id.minTemp)).setText(temp_min);
+                            ((TextView) findViewById(R.id.maxTemp)).setText(temp_max);
 
 
                             ((TextView) findViewById(R.id.sunrise)).setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH)
                                     .format(new Date(sunrise * 1000)));
 
                             ((TextView) findViewById(R.id.sunset)).setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH)
-                                    .format(new Date(sunset  * 1000)));
+                                    .format(new Date(sunset * 1000)));
 
-                            ((TextView) findViewById(R.id.wind)).setText(wind1 + " km/h");
+                            ((TextView) findViewById(R.id.wind)).setText(windd);
                             ((TextView) findViewById(R.id.pressure)).setText(pressure);
-                            ((TextView) findViewById(R.id.humidity)).setText(humidity + "%");
+                            ((TextView) findViewById(R.id.humidity)).setText(humidity);
 
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        } catch (IOException | JSONException e) {
+                            String error = "Bład danych";
+                            ((TextView) findViewById(R.id.location)).setText(error);
                         }
                     }
-                },
-
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        );
-        requestQueue.add(objectRequest);
-
-
-//        getCity = (EditText) findViewById(R.id.enterLocation);
-//        click = (Button) findViewById(R.id.searchLocation);
-
-
-//        click.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                city = getCity.getText().toString();
-//
-//            }
-//        });
-
-
+                });
+            }
+        });
     }
-
-
 }
+                    }
--- app/src/main/java/com/example/relativelayoutlearning/WeatherRepository.java
+++ app/src/main/java/com/example/relativelayoutlearning/WeatherRepository.java
@@ -1,102 +1,100 @@
 //package com.example.relativelayoutlearning;
 //
+//import android.app.Activity;
 //import android.content.Context;
 //import android.util.Log;
+//import android.widget.EditText;
 //import android.widget.TextView;
+//
+//
 //import androidx.appcompat.app.AppCompatActivity;
-//import com.android.volley.Request;
-//import com.android.volley.RequestQueue;
-//import com.android.volley.Response;
-//import com.android.volley.VolleyError;
-//import com.android.volley.toolbox.JsonObjectRequest;
-//import com.android.volley.toolbox.Volley;
+//import okhttp3.*;
+//import org.jetbrains.annotations.NotNull;
 //import org.json.JSONException;
 //import org.json.JSONObject;
 //
+//import java.io.IOException;
 //import java.text.SimpleDateFormat;
+//import java.util.ArrayList;
 //import java.util.Date;
 //import java.util.Locale;
 //
 //public class WeatherRepository extends AppCompatActivity {
 //
-//    private static String city = "Rokietnica,PL";
 //    private static final String API = "8544c10e7dc1de8a48fd3f18dd3c13ed";
 //
-//    protected void getWeather () {
+//    public String temp_min;
+//    public String temp_max;
+//    public String pressure;
+//    public String humidity;
 //
-//        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&lang=pl&appid=" + API;
-//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//    private String location;
+//    private String temp;
+//    private Long sunset;
+//    private Long sunrise;
+//    private String windd;
+//    public String description;
+//    public String lastModified = "fafa";
 //
-//        JsonObjectRequest objectRequest = new JsonObjectRequest(
-//                Request.Method.GET,
-//                url,
-//                null,
-//                new Response.Listener<JSONObject>() {
-//                    public void onResponse (JSONObject response) {
+//    protected void getWebservice(String city) {
+//
+//        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=pl&appid=" + API;
+//        final Request request = new Request.Builder().url(url).build();
+//
+//        OkHttpClient client = new OkHttpClient();
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        String error = "Brak internetu";
+//
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onResponse(@NotNull Call call, @NotNull Response response) {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
 //                        try {
-//                            JSONObject main = response.getJSONObject("main");
-//                            JSONObject sys = response.getJSONObject("sys");
-//                            JSONObject wind = response.getJSONObject("wind");
-//                            JSONObject weather = response.getJSONArray("weather").getJSONObject(0);
+//                            String jsonData = response.body().string();
+//                            JSONObject jObject = new JSONObject(jsonData);
 //
-//                            String lastModified = new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH)
-//                                    .format(new Date(response.getLong("dt") * 1000));
+//                            JSONObject main = jObject.getJSONObject("main");
+//                            JSONObject sys = jObject.getJSONObject("sys");
+//                            JSONObject wind = jObject.getJSONObject("wind");
+//                            JSONObject weather = jObject.getJSONArray("weather").getJSONObject(0);
 //
-//
+//                            lastModified = "Ostatnia akutlizacja: " + (new SimpleDateFormat("dd/MM/yyyy hh:mm a",
+//                                    Locale.ENGLISH).format(new Date(jObject.getLong("dt") * 1000)));
 //
 //                            //main
-//                            Long temp = Math.round(main.getDouble("temp"));
-//                            String temp_min = main.getString("temp_min");
-//                            String temp_max = main.getString("temp_max");
-//                            String pressure = main.getString("pressure");
-//                            String humidity = main.getString("humidity");
+//                            temp = (Math.round(main.getDouble("temp"))) + "°C";
+//                            temp_min = "Min temp: " + (main.getString("temp_min")) + "°C";
+//                            temp_max = "Max temp: " + (main.getString("temp_max")) + "°C";
+//                            pressure = main.getString("pressure");
+//                            humidity = (main.getString("humidity")) + " %";
 //
-//                            //sys
-//                            String location = response.getString("name") + ", " + sys.getString("country");
 //
-//                            long sunrise = sys.getLong("sunrise");
-//                            long sunset = sys.getLong("sunset");
+//                            location = jObject.getString("name") + ", " + sys.getString("country");
+//
+//                            sunrise = sys.getLong("sunrise");
+//                            sunset = sys.getLong("sunset");
 //
 //                            //wind
-//                            String wind1 = wind.getString("speed");
+//                            windd = (wind.getString("speed")) + " km/h";
 //                            //weather
-//                            String description = weather.getString("description");
+//                            description = weather.getString("description");
 //
-//                            ((TextView) findViewById(R.id.last_updated)).setText("Ostatnia aktualizacja: " +lastModified);
-//                            ((TextView) findViewById(R.id.location)).setText(location);
-//
-//                            ((TextView) findViewById(R.id.weatherDescription)).setText(description);
-//                            ((TextView) findViewById(R.id.temperature)).setText(temp + "°C");
-//
-//                            ((TextView) findViewById(R.id.minTemp)).setText("Min temp: " + temp_min + "°C");
-//                            ((TextView) findViewById(R.id.maxTemp)).setText("Max temp: " + temp_max + "°C");
-//
-//
-//                            ((TextView) findViewById(R.id.sunrise)).setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH)
-//                                    .format(new Date(sunrise * 1000)));
-//
-//                            ((TextView) findViewById(R.id.sunset)).setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH)
-//                                    .format(new Date(sunset  * 1000)));
-//
-//                            ((TextView) findViewById(R.id.wind)).setText(wind1 + " km/h");
-//                            ((TextView) findViewById(R.id.pressure)).setText(pressure);
-//                            ((TextView) findViewById(R.id.humidity)).setText(humidity + "%");
-//
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
+//                        } catch (IOException | JSONException e) {
+//                            String error = "Zła lokalizacja";
 //                        }
 //                    }
-//                },
-//
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//
-//                    }
-//                }
-//        );
-//        requestQueue.add(objectRequest);
-//
+//                });
+//            }
+//        });
 //    }
 //}
